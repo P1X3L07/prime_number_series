67ff84eac047a0212b7cb7da4d0b9c3c7648f701
P1X3L07 <patricialowis2@gmail.com>
2021-12-03 10:38:32 +0700

    Add project files.

---
+++ prime number series/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime_number_series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prime: ");
+            int Prime = Convert.ToInt32(Console.ReadLine());
+
+            if(Prime > 0 && Prime % 2 == 1)
+            {
+                Console.WriteLine("It is a prime num");
+            }
+            else
+            {
+                Console.WriteLine("Not a prime num");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
